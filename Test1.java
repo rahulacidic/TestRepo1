b101c0f9eab76277539482ad8121d63e35d31cf7
Rahul <40708840+rahulacidic@users.noreply.github.com>
2019-06-30 20:50:48 +0530

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -1,5 +1,7 @@
 class HelloWorld{
 public static void main(String args[]){
 System.out.println("Helllo World");
+System.out.println("This is added1");
+System.out.println("This is also same");  
 }
 }
